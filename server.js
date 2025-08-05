0622e7273440b129c5affe15d1a8b32f39b2d32f
WeremyJalters <jeremyjwalters@gmail.com>
2025-08-05 13:36:57 -0400

    Update server.js
    remove debug lines

--- server.js
+++ server.js
@@ -81,10 +81,7 @@ const conferenceRooms = new Map();
 app.post('/webhooks/calls', async (req, res) => {
     const { data } = req.body;
     
-    // DEBUG: Log the entire webhook payload
-    console.log('=== FULL WEBHOOK DATA ===');
-    console.log(JSON.stringify(req.body, null, 2));
-    console.log('=== END WEBHOOK DATA ===');
+   
     
     const callId = data.payload?.call_control_id || data.call_control_id;
     
